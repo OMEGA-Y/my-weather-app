f343f47135f5cdb53cf673f68b272a91e620ed32
OMEGA-Y <science_co@naver.com>
2021-05-18 18:10:25 +0900

    add CityListHeader component

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,13 +1,15 @@
+import CityListHeader from "./CityListHeader";
+import "./CityList.css";
 function CityList(props) {
   // const cities = props.cities;
   const { cities } = props;
 
   return (
     <div>
-      <h1>CityList</h1>
-      <ul>
+      <CityListHeader />
+      <ul className="citylist">
         {cities.map((item, index) => {
-          return <li kye={index}>{item}</li>;
+          return <li key={index}>{item}</li>;
         })}
       </ul>
     </div>
---
+++ src/components/CityListHeader.jsx
@@ -0,0 +1,10 @@
+function CityListHeader() {
+  return (
+    <div>
+      <h1>CityList</h1>
+      <p>It's mock data. You can see it freely.</p>
+    </div>
+  );
+}
+
+export default CityListHeader;
